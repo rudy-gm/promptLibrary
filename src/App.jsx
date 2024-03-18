3e9a1a2bc632cbe277f83e8f06f29fc855393e2b
rudy-gm <rodolfo-gm@hotmail.com>
2024-03-18 00:25:36 -0400

    Firs commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Home from "./pages/Home";
+import Pages from "./pages/Pages";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <Pages></Pages>
+    </div>
+  );
+}
+
+export default App;
