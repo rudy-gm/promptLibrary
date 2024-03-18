f9b5b1770d8e92cc434e450d4968a42b02811427
rudy-gm <rodolfo-gm@hotmail.com>
2024-03-18 01:40:04 -0400

    Second commit // basics for popular and veggie components

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
-import styled from "styled-components";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import '@splidejs/react-splide/css';
+import {Wrapper, Card, Gradient} from "./styledComponents"
 
 function Popular() {
 
@@ -12,10 +12,25 @@ function Popular() {
     }, [])
 
     const getPopular = async () => {
-        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
-        const data = await api.json()
-        setPopular(data.recipes)
-    }
+
+        const check = localStorage.getItem('popular');
+
+        if(check){
+            setPopular(JSON.parse(check));
+        
+        }
+        else{
+
+            const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
+
+            const data = await api.json()
+
+            localStorage.setItem('popular', JSON.stringify(data.recipes));
+            setPopular(data.recipes)
+
+        }
+        
+    };
 
     return (
         <div>
@@ -31,11 +46,11 @@ function Popular() {
                 }}>
                     {popular.map((recipe) => {
                         return (
-                            <SplideSlide>
+                            <SplideSlide key={recipe.id}>
                                 <Card>
                                     <p>{recipe.title}</p>
-                                    <p>Hello</p>
                                     <img src={recipe.image} alt={recipe.title} />
+                                    <Gradient></Gradient>
                                 </Card>
                             </SplideSlide>
                         );
@@ -46,44 +61,5 @@ function Popular() {
         </div>
     )
 }
-
-const Wrapper = styled.div`
-    margin: 4rem 0rem
-`;
-
-const Card = styled.div`
-    min-height: 25rem;
-    border-radius: 2rem;
-    overflow: hidden;
-
-    img{
-        border-radius: 2rem;
-        position: absolute;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-    }
-
-    p{
-        position: absolute;
-        z-index: 10;
-        left: 50%;
-        bottom: 0%;
-        transform: translate(-50%, 0%);
-        color: white; 
-        width: 100%;
-        text-align: center;
-        font-weight:600;
-        font-size: 1rem; 
-        height: 40%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-
-
-    }
-
-`;
 
 export default Popular
--- src/components/Veggie.jsx
+++ src/components/Veggie.jsx
@@ -1,7 +1,67 @@
+import {Wrapper, Card, Gradient} from "./styledComponents"
+import { useEffect, useState } from "react";
+import '@splidejs/react-splide/css';
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+
 function Veggie() {
-  return (
-    <div>Veggie</div>
-  )
+
+    const [veggie, setVeggie] = useState([])
+
+    useEffect(() => {
+        getVeggie();
+    }, [])
+
+    const getVeggie = async () => {
+
+        const check = localStorage.getItem('veggie');
+
+        if(check){
+            setVeggie(JSON.parse(check));
+        
+        }
+        else{
+
+            const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9&tags=vegetarian`);
+
+            const data = await api.json()
+
+            localStorage.setItem('veggie', JSON.stringify(data.recipes));
+            setVeggie(data.recipes)
+
+        }
+
+        console.log(veggie)
+        
+    };
+
+    return (
+        <div>
+            <Wrapper>
+                <h3>Our Vegetarian Picks</h3>
+
+                <Splide options={{
+                    perPage: 3,
+                    arrows: false,
+                    pagination: false,
+                    drag: 'free',
+                    gap: '5rem',
+                }}>
+                    {veggie.map((recipe) => {
+                        return (
+                            <SplideSlide key={recipe.id}>
+                                <Card>
+                                    <p>{recipe.title}</p>
+                                    <img src={recipe.image} alt={recipe.title} />
+                                    <Gradient></Gradient>
+                                </Card>
+                            </SplideSlide>
+                        );
+                    })}
+                </Splide>
+            </Wrapper>
+
+        </div>
+    )
 }
 
 export default Veggie
